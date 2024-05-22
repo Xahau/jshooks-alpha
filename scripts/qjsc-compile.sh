6507c7a5115645fda203eff5c3a165a7e4936c51
Wietse Wind <mail@wietse.com>
2024-05-22 23:44:15 +0200

    Switch to esbuild

--- scripts/qjsc-compile.sh
+++ scripts/qjsc-compile.sh
@@ -7,7 +7,7 @@ input_file="$1"
 output_file="/build/${input_file%.js}.bc"
 
 # Strip imports, esmodule, etc.
-cat "/contracts/$input_file" |grep -v -E 'Object.defineProperty.+exports.+__esModule'|grep -v -E '^exports\..+=.+' > temp.js && mv temp.js "/contracts/$input_file"
+cat "/contracts/$input_file" |grep -v -E 'Object.defineProperty.+exports.+__esModule'|grep -v -E '^exports\..+=.+'|sed 's/^export.*{/const _skip_export = {/g' > temp.js && mv temp.js "/contracts/$input_file"
 
 # Generate the hex output and convert it to bytes
 hex_output=$(/quickjslite/qjsc -c -o /dev/stdout "/contracts/$input_file" | tr -d '\n' | grep -Eo '\{[^}]+\}' | grep -Eo '0x[a-fA-F0-9]+' | sed -e 's/0x//g' | tr -d '\n')
